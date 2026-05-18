ae534bd69b50fdb6163b3d0a4d9986db236e7742
Minion Ngawi <achmadrianta4@gmail.com>
2026-05-18 08:10:57 +0000

    comj

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -4,4 +4,11 @@ using namespace std;
 class seseorang{
 public:
     virtual void pesan() = 0;
+};
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko " << endl;
+    }
 }
