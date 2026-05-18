98323c5c0848d7a038498ed41a60a41dfc1fe398
Minion Ngawi <achmadrianta4@gmail.com>
2026-05-18 07:59:02 +0000

    barang 17 e

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -34,11 +34,15 @@ class budi : public pekerja, public pelajar {
 public: 
    budi(int pUmur):
        pekerja(pUmur),
-       pelajar(pUmur),
-       orang(pUmur)
+       pelajar(pUmur)
     {
-        cpout
+        cout << "Budi dibuat\n" << endl;
     }
 
+};
+
+int main() {
+    budi a(12);
+    return 0;
 }
 
